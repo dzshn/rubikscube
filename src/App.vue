0cf4e72bd0b4cdb6762db6dcd79c7a41612e293d
dzshn <zshn@pm.me>
2021-02-14 22:18:26 -0300

    add footer links

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,9 @@
     <h1>cube</h1>
     <Cube/>
     <footer>
-        <p>@dzshn 2021 | made with vue.js and three.js</p>
+        <p>
+            <a href="https://github.com/dzshn/rubikscube/blob/master/LICENSE">MIT License</a> ─ <a href="https://github.com/dzshn">dzshn</a> | made with <a href="https://vuejs.org">vue.js</a> and <a href="https://threejs.org">three.js</a>
+        </p>
     </footer>
 </template>
 
@@ -28,8 +30,19 @@ body {
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     text-align: center;
-    color: #fff;
+    color: #aaa;
     overflow: none;
+}
+
+h1, a {
+    color: #fff
+}
+
+a {
+    text-decoration: none;
+    background: #3a3a3a;
+    padding: 3px 5px 1px;
+    border-radius: 5px;
 }
 
 footer {
